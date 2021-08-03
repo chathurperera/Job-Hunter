911b375bfac769341ee49413e4f6bdf3b461c852
chathura <chathuraperera007@gmail.com>
2021-08-03 23:25:47 +0530

    added data

--- src/data.js
+++ src/data.js
@@ -9,7 +9,18 @@ const vacencies = [
         experience:'min 2 years industrial experience',
         location:'Colombo',
         salary:'LKR 20,000 - 40,000/month',
-        industry:'Information & Communication Technology'
+        industry:'Information & Communication Technology',
+        desc:'We are looking for a UI/UX Designer to turn our software into easy-to-use products for our clients. UI/UX Designer responsibilities include gathering user requirements, designing graphic elements and building navigation components. To be successful in this role, you should have experience with design software and wireframing tools. If you also have a portfolio of professional design projects that includes work with web/mobile applications, we’d like to meet you. Ultimately, you’ll create both functional and appealing features that address our clients’ needs and help us grow our customer base.',
+        requirements:[
+            'Pursuing a degree in Design, Computer Science or relevant field',
+            'Portfolio of design projects',
+            'Knowledge of wireframe tools (e.g. Figma and Adobe XD)',
+            'Work with Visual Designers to evolve our brand identity style guide',
+            'Up-to-date knowledge of design software like Adobe Illustrator and Photoshop',
+            'Team spirit; strong communication skills to collaborate with various stakeholders',
+            'Good time-management skills'
+        ]
+        
     },
     {
         id:2,
@@ -20,7 +31,17 @@ const vacencies = [
         experience:'min 4 years industrial experience',
         location:'Colombo',
         salary:'LKR 50,000 - 150,000/month',
-        industry:'Information & Communication Technology'
+        industry:'Information & Communication Technology',
+        desc:'Our client is a reputed software company developing and delivering solutions for a diverse set of business entities including ERP Systems, Finance Systems, HR Systems, and Sales Force Automation (SFA) and more.',
+        requirements:[
+            'Bachelor’s degree in Computer Science or equivalent qualification',
+            'Minimum 2+ years of web-based application development experience in C# / .NET',
+            'Solid knowledge of Object-Oriented Analysis and Design, Software design patterns, and best practices',
+            'Familiarity with front-end frameworks (JQuery, React)',
+            'Proficient in .NET technologies. (.NET Core/Framework, ASP.NET Razor/MVC/Web API)',
+            'Experience in MSSQL Database Activities',
+            'Knowledge of HTML 5, CSS, and JavaScript'
+        ]
     },
     {
         id:3,
@@ -31,7 +52,18 @@ const vacencies = [
         experience:'min 2 years industrial experience',
         location:'Colombo',
         salary:'LKR 40,000 - 50,000/month',
-        industry:'Information & Communication Technology'
+        industry:'Information & Communication Technology',
+        desc:'Viable is a company focusing on digital media marketing and strategies, invested in creating a unique digital and social media presence for companies and organisations. We aim at achieving the most sustainable and possibly the best solution for your brands digital media marketing campaign.',
+        requirements:[
+            'Execute web audit, collect and analyse data and results, identify gaps, trends and insights and work towards improving organic search results and ROI maximisation.',
+            'Plan, develop and implement a search engine optimisation strategy across clients.',
+            'Identification of KPIs to improve search results.',
+            'Be well- versed with on-page optimisation (e.g., title tags, alt tags, H1, copy optimization)',
+            'Regularly carry out keyword research and content optimisation activities to help improve overall campaign performance and drive better results.',
+            'Take ownership of Search Console to understand changes across campaigns and formulate new strategies to improve ranking across different keywords.',
+            'Assist with campaign reporting: performance reports, campaign analytics, ad-hoc analysis and conversion optimisation.',
+            'Collaborate with the development team, content team and design team to implement strategy.',
+        ]
     },
     {
         id:4,
@@ -42,7 +74,18 @@ const vacencies = [
         experience:'min 4 years industrial experience',
         location:'Colombo',
         salary:'LKR 15,000 - 20,000/month',
-        industry:'Information & Communication Technology'
+        industry:'Information & Communication Technology',
+        desc:'This role is scheduled predominantly during Monday -Friday. This is a part-time position which reports to the Manager, Marketing Communications. The position will be remote. it may require partial on-site requirements in future.',
+        requirements:[
+            'Develop, implement and manage our social media strategy',
+            "Measure the success of every social media campaign with KPI's",
+            'Stay up to date with the latest social media best practices and technologies',
+            'Use social media marketing tools such as Buffer',
+            'Work with copywriters and designers to ensure content is informative and appealing',
+            'Collaborate with Marketing, Sales and Product Development teams',
+            'Monitor SEO and user engagement and suggest content optimization',
+            'Communicate with industry professionals and influencers via social media to create a strong network'
+        ]
     },
     {
         id:5,
@@ -53,7 +96,15 @@ const vacencies = [
         experience:'min 3 years industrial experience',
         location:'Colombo',
         salary:'LKR 30,000 - 60,000/month',
-        industry:'Information & Communication Technology'
+        industry:'Information & Communication Technology',
+        desc:'The Senior Project Manager is a complex, high-profile, and client-facing role in which you are leading and managing the timeline, process, and administration of each assignment. Additionally, you will support business development and be an integral part of the team, both in your office and/or practice, but also the greater FYXO team. The successful candidate is self-motivated/a self-starter, flexible, and able to manage multiple projects and changing priorities in a fast-paced environment. S/he must ensure discreet handling of all interactions and communications with clients, candidates, etc., and be able to work both autonomously and collaboratively.',
+        requirements:[
+            'Bachelor’s Degree with minimum 5 years in a support role at the Executive level.',
+            'Self-Starter with a can-do attitude.',
+            'Good organization and time management skills.',
+            'Ability to learn quickly.',
+            'Impeccable integrity; team player.'
+        ]
     },
     {
         id:6,
@@ -64,18 +115,36 @@ const vacencies = [
         experience:'min 4 years industrial experience',
         location:'Colombo',
         salary:'LKR 15,000 - 20,000/month',
-        industry:'Information & Communication Technology'
+        industry:'Information & Communication Technology',
+        desc:'We are looking for an Intern to join our HR department, to learn, help us in day to days affairs and helps in enhancing the team at Hex Wireless Pvt Ltd for our exclusive product Power SMPP. The position is based on Vadodara, India and is a day shift position.',
+        requirements:[
+            'Graduate, post graduate with HR specialization, BSW, MSW etc.',
+            'Personable, able to comfortably and pleasantly deal with a variety of people',
+            'Strong customer service skills',
+            'Exceptional written and oral communication skills',
+            'Ability to effectively learn and acquire new knowledge and skills.',
+            'Ability to share knowledge and work in a strong team-oriented environment.'
+        ]
     },
     {
         id:7,
-        title:'Esports Social Media Manage',
+        title:'Esports Social Media Manager',
         type:'Contract',
         company:'InGame Entertainment PVT LTD',
         comLogo:'https://roosterpaladinstoragebucket.s3.amazonaws.com/companyLogos/logo_1590780354405_crabby-face.png',
         experience:'min 1 years industrial experience',
         location:'Colombo',
         salary:'LKR 20,000 - 30,000/month',
-        industry:'Information & Communication Technology'
+        industry:'Information & Communication Technology',
+        desc:'This role will be responsible for working with the media team to create engaging Esports content. The content required will span audio, video and text mediums for news, reviews, community building and marketing purposes. The content will be distributed primarily on digital, with some content being used on non-digital platforms.',
+        requirements:[
+            'Leading the content team',
+            'Working with graphics designers, video editors and multi-language writers holistically to output complimentary content',
+            'Creating content strategies for Esports initiatives',
+            'Has a strong interest in Esports',
+            'Prior experience in a professional digital media capacity',
+            'Preferably has experience in running paid digital campaigns '
+        ]
     },
 
 ]
